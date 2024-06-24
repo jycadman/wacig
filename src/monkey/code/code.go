e2426f24b6f733d553376b83bb8609c22c1d1589
jycadman <jycadman@unm.edu>
2024-06-24 12:28:45 -0600

    Extended code package with  opcode

--- src/monkey/code/code.go
+++ src/monkey/code/code.go
@@ -1,5 +1,3 @@
-// code/code.go
-
 package code
 
 import (
@@ -40,6 +38,8 @@ func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
 	}
 
 	switch operandCount {
+	case 0:
+		return def.Name
 	case 1:
 		return fmt.Sprintf("%s %d", def.Name, operands[0])
 	}
@@ -51,6 +51,7 @@ type Opcode byte
 
 const (
 	OpConstant Opcode = iota
+	OpAdd
 )
 
 type Definition struct {
@@ -60,6 +61,7 @@ type Definition struct {
 
 var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}},
+	OpAdd:      {"OpAdd", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
