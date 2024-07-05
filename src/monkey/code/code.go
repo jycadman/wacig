a1b3b611437b8ed70a87a38c7710d216955e492a
jycadman <jycadman@unm.edu>
2024-07-05 16:32:59 -0600

    Extended compiler to compile basic conditional statement

--- src/monkey/code/code.go
+++ src/monkey/code/code.go
@@ -69,6 +69,9 @@ const (
 
 	OpMinus
 	OpBang
+
+	OpJumpNotTruthy
+	OpJump
 )
 
 type Definition struct {
@@ -96,6 +99,9 @@ var definitions = map[Opcode]*Definition{
 
 	OpMinus: {"OpMinus", []int{}},
 	OpBang:  {"OpBang", []int{}},
+
+	OpJumpNotTruthy: {"OpJumpNotTruthy", []int{2}},
+	OpJump:          {"OpJump", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
