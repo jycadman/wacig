5e3dd9c965b4891991ee999d85d19f5fd962f65c
jycadman <jycadman@unm.edu>
2024-07-12 15:52:13 -0600

    Extended compiler to compile index expressions

--- src/monkey/code/code.go
+++ src/monkey/code/code.go
@@ -79,8 +79,8 @@ const (
 	OpSetGlobal
 
 	OpArray
-
 	OpHash
+	OpIndex
 )
 
 type Definition struct {
@@ -119,6 +119,7 @@ var definitions = map[Opcode]*Definition{
 
 	OpArray: {"OpArray", []int{2}},
 	OpHash:  {"OpHash", []int{2}},
+	OpIndex: {"OpIndex", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
